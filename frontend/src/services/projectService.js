1f552914a96719855b96e3a368c5df3d24363510
Niraj Singh <niraj1507singh@gmail.com>
2026-02-06 15:51:43 +0530

    bug fix in edit project module

--- frontend/src/services/projectService.js
+++ frontend/src/services/projectService.js
@@ -5,17 +5,22 @@ export const getProjects = async () => {
   return res.data;
 };
 
+export const getRootProjects = async () => {
+  const res = await api.get("/api/project/root-projects");
+  return res.data;
+};
+
 export const getProjectById = async (id) => {
   const res = await api.get(`/api/project/${id}`);
   return res.data;
 };
 
 export const createProject = async (payload) => {
-  return api.post("/api/project", payload);
+  return api.post("/api/project/add", payload);
 };
 
 export const updateProject = async (id, payload) => {
-  return api.put(`/api/project/${id}`, payload);
+  return api.put(`/api/project/${id}/edit`, payload);
 };
 
 export const deleteProject = async (id) => {
