3878d496fcfc8cd6f99dc3897d8e5688cf43094f
Niraj Singh <niraj1507singh@gmail.com>
2026-02-10 18:38:31 +0530

    redux toolkit setup for auth and bug fix in getProject Api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,7 @@
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { loginSuccess } from "./store/slices/authSlice";
 import Login from "./pages/Login";
 import MainLayout from "./layout/MainLayout";
 import Projects from "./pages/project/Projects";
@@ -8,6 +11,20 @@ import AddProject from "./pages/project/AddProject";
 import EditProject from "./pages/project/EditProject";
 
 export default function App() {
+   const dispatch = useDispatch();
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const user = localStorage.getItem("user");
+
+    if (token && user) {
+      dispatch(
+        loginSuccess({
+          token,
+          user: JSON.parse(user),
+        })
+      );
+    }
+  }, [dispatch]);
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,22 +1,15 @@
-/*import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)*/
-
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import App from "./App";
+import store from "./store";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  
 );
